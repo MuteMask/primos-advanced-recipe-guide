01894cbc90fa9846b5bdd8159588a8834b13619f
MuteMask <dvinzlee@gmail.com>
2026-04-15 22:40:19 +0800

    Update CraftHereButton.java

--- src/main/java/com/mutemask/primorecipe/client/gui/widget/CraftHereButton.java
+++ src/main/java/com/mutemask/primorecipe/client/gui/widget/CraftHereButton.java
@@ -8,9 +8,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 
 public class CraftHereButton extends Button {
-    private static final ResourceLocation BUTTON_TEXTURE = ResourceLocation.parse("textures/gui/container/crafting_table.png");
-    private static final int BUTTON_WIDTH = 70;
-    private static final int BUTTON_HEIGHT = 32; // 2 inventory slots height
+    private static final ResourceLocation BUTTON_TEXTURE = ResourceLocation.withDefaultNamespace("textures/gui/container/crafting_table.png");
     
     public CraftHereButton(int x, int y, int width, int height, Component message, OnPress onPress) {
         super(x, y, width, height, message, onPress, DEFAULT_NARRATION);
@@ -23,18 +21,14 @@ public class CraftHereButton extends Button {
         RenderSystem.enableBlend();
         RenderSystem.defaultBlendFunc();
         
-        // Draw button background (using vanilla crafting table texture or custom)
-        int v = this.isHovered ? 32 : 0; // Hover state offset
+        int v = this.isHovered ? 32 : 0;
         
-        // Draw the button as a 2-slot high button
         guiGraphics.blit(BUTTON_TEXTURE, this.getX(), this.getY(), 0, 166, this.width, 16);
         guiGraphics.blit(BUTTON_TEXTURE, this.getX(), this.getY() + 16, 0, 166, this.width, 16);
         
-        // Draw button border
-        int borderColor = this.isHovered ? 0xFF00FF00 : 0xFFFFFFFF; // Green when hovered, white normally
+        int borderColor = this.isHovered ? 0xFF00FF00 : 0xFFFFFFFF;
         guiGraphics.renderOutline(this.getX(), this.getY(), this.width, this.height, borderColor);
         
-        // Draw "Craft Here" text centered
         String text = "Craft Here";
         int textWidth = guiGraphics.font.width(text);
         int textX = this.getX() + (this.width - textWidth) / 2;
