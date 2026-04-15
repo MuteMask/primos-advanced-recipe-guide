32890c62237c6affc07de722d3afe74dc2bd0d64
MuteMask <dvinzlee@gmail.com>
2026-04-15 22:39:06 +0800

    Update SearchableCraftingScreen.java

--- src/main/java/com/mutemask/primorecipe/client/gui/SearchableCraftingScreen.java
+++ src/main/java/com/mutemask/primorecipe/client/gui/SearchableCraftingScreen.java
@@ -1,11 +1,9 @@
 package com.mutemask.primorecipe.client.gui;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import com.mutemask.primorecipe.client.gui.widget.SearchBarWidget;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.gui.screens.inventory.CraftingScreen;
-import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
@@ -17,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SearchableCraftingScreen extends AbstractContainerScreen<CraftingMenu> {
-    private static final ResourceLocation CRAFTING_TABLE_LOCATION = ResourceLocation.parse("textures/gui/container/crafting_table.png");
+    private static final ResourceLocation CRAFTING_TABLE_LOCATION = ResourceLocation.withDefaultNamespace("textures/gui/container/crafting_table.png");
     
     private SearchBarWidget searchBar;
     private boolean searchActive = false;
@@ -33,15 +31,13 @@ public class SearchableCraftingScreen extends AbstractContainerScreen<CraftingMe
     protected void init() {
         super.init();
         
-        // Position crafting table GUI in center
         this.leftPos = (this.width - this.imageWidth) / 2;
         this.topPos = (this.height - this.imageHeight) / 2;
         
-        // Create search bar - same width as GUI (176 pixels), positioned above
         int searchBarWidth = this.imageWidth;
         int searchBarHeight = 16;
         int searchBarX = this.leftPos;
-        int searchBarY = this.topPos - 25; // 25 pixels above the GUI
+        int searchBarY = this.topPos - 25;
         
         this.searchBar = new SearchBarWidget(
             this.font,
@@ -59,35 +55,22 @@ public class SearchableCraftingScreen extends AbstractContainerScreen<CraftingMe
 
     @Override
     public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
-        // Render background
         this.renderBackground(guiGraphics, mouseX, mouseY, partialTick);
         
-        // Render crafting table GUI
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        RenderSystem.setShaderTexture(0, CRAFTING_TABLE_LOCATION);
+        guiGraphics.blit(CRAFTING_TABLE_LOCATION, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight);
         
-        int x = this.leftPos;
-        int y = this.topPos;
-        guiGraphics.blit(CRAFTING_TABLE_LOCATION, x, y, 0, 0, this.imageWidth, this.imageHeight);
-        
-        // Render search bar
         this.searchBar.render(guiGraphics, mouseX, mouseY, partialTick);
         
-        // Render labels
-        guiGraphics.drawString(this.font, this.title, x + 28, y + 6, 4210752, false);
-        guiGraphics.drawString(this.font, this.playerInventoryTitle, x + 8, y + this.imageHeight - 94, 4210752, false);
+        guiGraphics.drawString(this.font, this.title, this.leftPos + 28, this.topPos + 6, 4210752, false);
+        guiGraphics.drawString(this.font, this.playerInventoryTitle, this.leftPos + 8, this.topPos + this.imageHeight - 94, 4210752, false);
         
-        // Render tooltips
         this.renderTooltip(guiGraphics, mouseX, mouseY);
     }
 
     @Override
     public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
-        // ESC is the only working keybind after typing starts
         if (keyCode == GLFW.GLFW_KEY_ESCAPE) {
             if (searchActive && !currentSearch.isEmpty()) {
-                // Clear search and go back to crafting table
                 currentSearch = "";
                 searchBar.setValue("");
                 searchActive = false;
@@ -97,7 +80,6 @@ public class SearchableCraftingScreen extends AbstractContainerScreen<CraftingMe
             return true;
         }
         
-        // Disable T key for search when in this screen
         if (keyCode == GLFW.GLFW_KEY_T && searchActive) {
             return true;
         }
@@ -118,7 +100,6 @@ public class SearchableCraftingScreen extends AbstractContainerScreen<CraftingMe
         searchActive = !text.isEmpty();
         
         if (searchActive && text.length() >= 1) {
-            // Transition to item search screen
             List<ItemStack> matchingItems = getMatchingItems(text);
             if (!matchingItems.isEmpty()) {
                 minecraft.setScreen(new ItemSearchScreen(
@@ -135,7 +116,6 @@ public class SearchableCraftingScreen extends AbstractContainerScreen<CraftingMe
 
     private List<ItemStack> getMatchingItems(String search) {
         List<ItemStack> results = new ArrayList<>();
-        // Get all craftable items from recipe manager
         var recipeManager = minecraft.level.getRecipeManager();
         var recipes = recipeManager.getRecipes();
         
@@ -156,7 +136,6 @@ public class SearchableCraftingScreen extends AbstractContainerScreen<CraftingMe
 
     @Override
     public boolean mouseClicked(double mouseX, double mouseY, int button) {
-        // Ensure search bar must be clicked manually
         if (searchBar.isMouseOver(mouseX, mouseY)) {
             searchBar.setFocused(true);
             return true;
@@ -173,7 +152,6 @@ public class SearchableCraftingScreen extends AbstractContainerScreen<CraftingMe
         this.leftPos = (this.width - this.imageWidth) / 2;
         this.topPos = (this.height - this.imageHeight) / 2;
         
-        // Reposition search bar
         searchBar.setX(this.leftPos);
         searchBar.setY(this.topPos - 25);
     }
